e8843439db8ad2c4712bcc6f62a26bfe3f257640
Alan <alantrawcz@gmail.com>
2021-12-10 19:24:07 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------------
+
+// PREAMBULO
+const author = "Alan Trawczynski";
+document.getElementById("author").innerHTML = author;
+
+// -----------------------------------------------------------------------
+
+// COMENTARIOS
+// ...
+
+// -----------------------------------------------------------------------
+
+// VARIABLES
+//    1. Acceso a elementos HTML con interacción (e interacciones asociadas)
+//    2. Acceso a elementos HTML informativos
+//    3. Uso general 
+// ...
+
+// -----------------------------------------------------------------------
+
+// FUNCIONES
+//    1. Start
+//    2. Auxiliares
+//    3. Gestores de eventos
+
+function start() {
+    console.log("starting...")
+}
+
+// -----------------------------------------------------------------------
+
+start()
