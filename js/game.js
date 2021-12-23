85a8ae9fbba219058b32f67fc96d1d4bd99bbba3
Alan <alantrawcz@gmail.com>
2021-12-23 16:58:01 +0100

    game logic in a separate class

---
+++ js/game.js
@@ -0,0 +1,189 @@
+class Game {
+  constructor(container) {
+    this.container = container;
+    this.isSaved = localStorage.getItem("boardValues") !== null;
+    this.isGameover = false;
+
+    this.boardValues = null;
+    this.board = null;
+    this.score = null;
+    this.size = null;
+
+    if (this.isSaved) {
+      this.load();
+    } else {
+      this.newGame(4);
+    }
+  }
+
+  // Loads and initializes game stored in localStorage
+  load() {
+    this.boardValues = JSON.parse(localStorage.getItem("boardValues"));
+    this.score = +localStorage.getItem("score");
+    this.size = this.boardValues.length;
+    this.generateBoard();
+    this.checkGameover();
+  }
+
+  // Saves game in localStorage
+  save() {
+    localStorage.setItem("boardValues", JSON.stringify(this.boardValues));
+    localStorage.setItem("score", this.score);
+    this.isSaved = true;
+  }
+
+  // Creates a new game with given size
+  newGame(size) {
+    this.container.innerHTML = ""; // Removes all child nodes of board container
+    this.isGameover = false;
+    this.score = 0;
+    this.size = size;
+    this.boardValues = Array(size) // Set boardValues to a matrix of 0s
+      .fill(0)
+      .map((_) => Array(size).fill(0));
+
+    this.generateBoard(); // Generate HTML board elements
+    this.fillSquare();
+    this.fillSquare();
+    this.save();
+  }
+
+  // Generates a board using boardValues data
+  generateBoard() {
+    this.board = this.boardValues.map((row) =>
+      row.map((value) => {
+        const square = document.createElement("div");
+        square.innerHTML = value !== 0 ? value : "";
+        this.container.appendChild(square);
+        return square;
+      })
+    );
+  }
+
+  // Randomly fills an empty position (square) on the board
+  fillSquare() {
+    const row = Math.floor(Math.random() * this.size),
+      col = Math.floor(Math.random() * this.size),
+      squareValue = this.boardValues[row][col];
+
+    if (squareValue === 0) {
+      this.setSquare(row, col, 2, true);
+    } else this.fillSquare();
+  }
+
+  // Updates a single square of board with the given value
+  setSquare(row, col, value) {
+    this.boardValues[row][col] = value;
+    this.board[row][col].innerHTML = value !== 0 ? value : "";
+  }
+
+  moveRight() {
+    this.move("R");
+  }
+  moveLeft() {
+    this.move("L");
+  }
+  moveUp() {
+    this.move("U");
+  }
+  moveDown() {
+    this.move("D");
+  }
+
+  // Moves the squares in `dir` direction
+  move(dir, combine = true) {
+    let moved = false;
+    const isHorizontal = ["R", "L"].includes(dir),
+      vectors = isHorizontal ? this.boardValues : transpose(this.boardValues);
+
+    for (const [i, vector] of vectors.entries()) {
+      let newVector = vector.filter((n) => n);
+      const missingValues = Array(this.size - newVector.length).fill(0);
+      newVector = ["R", "D"].includes(dir)
+        ? missingValues.concat(newVector)
+        : newVector.concat(missingValues);
+
+      for (const [j, value] of vector.entries()) {
+        const newValue = newVector[j],
+          [row, col] = isHorizontal ? [i, j] : [j, i];
+
+        if (value !== newValue) {
+          this.setSquare(row, col, newValue);
+          moved = true;
+        }
+      }
+    }
+
+    if (combine) {
+      const combined = this.combineVectors(dir);
+      if (moved || combined) {
+        this.fillSquare();
+        this.checkGameover();
+        this.save();
+      }
+    }
+  }
+
+  // Combines rows or columns after each move
+  combineVectors(dir) {
+    let combined = false;
+    const isHorizontal = ["R", "L"].includes(dir),
+      vectors = isHorizontal ? this.boardValues : transpose(this.boardValues),
+      [start, end, step] = ["L", "U"].includes(dir)
+        ? [0, this.size - 1, 1]
+        : [this.size - 1, 0, -1];
+
+    for (let [i, vector] of vectors.entries()) {
+      for (let j = start; j !== end; j += step) {
+        const value = vector[j],
+          nextValue = vector[j + step];
+
+        if (value !== 0 && value === nextValue) {
+          const [row, col] = isHorizontal ? [i, j] : [j, i];
+          const [nextRow, nextCol] = isHorizontal
+            ? [i, j + step]
+            : [j + step, i];
+
+          vector[j] *= 2;
+          vector[j + step] = 0;
+          this.setSquare(row, col, value * 2);
+          this.setSquare(nextRow, nextCol, 0);
+          this.score += value * 2;
+          combined = true;
+        }
+      }
+    }
+
+    if (combined) {
+      this.move(dir, false);
+    }
+    return combined;
+  }
+
+  checkGameover() {
+    // Check for combinable squares
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        const current = this.boardValues[row][col],
+          neighbors = [
+            this.boardValues[row + 1]?.[col],
+            this.boardValues[row - 1]?.[col],
+            this.boardValues[row]?.[col + 1],
+            this.boardValues[row]?.[col - 1],
+          ].filter((n) => n);
+
+        // console.log(neighbors, current);
+        if (current === 0 || neighbors.includes(current)) {
+          return;
+        }
+      }
+    }
+
+    this.isGameover = true;
+  }
+}
+
+// Transposes the input matrix
+function transpose(M) {
+  return M[0].map((_, i) => M.map((row) => row[i]));
+}
---
+++ js/script.js
@@ -0,0 +1,103 @@
+"use strict";
+
+// -----------------------------------------------------------------------
+
+// PREAMBULO
+const author = "Alan Trawczynski";
+document.getElementById("author").innerHTML = author;
+
+// -----------------------------------------------------------------------
+
+// COMENTARIOS
+// ...
+
+// -----------------------------------------------------------------------
+
+// VARIABLES
+//    1. Acceso a elementos HTML con interacción (e interacciones asociadas)
+//    2. Acceso a elementos HTML informativos
+//    3. Uso general
+const boardDisplay = document.getElementById("board"),
+  newGameButton = document.getElementById("newGameButton"),
+  sizeSlider = document.getElementById("boardSizeSlider");
+
+const sizeDisplay = document.getElementById("boardSizeValue"),
+  scoreDisplay = document.getElementById("score");
+
+let game = new Game(boardDisplay);
+const boardDisplaySize = 0.6; // [0, 1]: portion of window size
+
+// -----------------------------------------------------------------------
+
+// FUNCIONES
+//    1. Start
+//    2. Auxiliares
+//    3. Gestores de eventos
+
+function start() {
+  console.log("starting...");
+  addEventListeners();
+  resizeBoard();
+  updateDisplay();
+}
+
+function addEventListeners() {
+  document.addEventListener("keyup", controlKeyUp);
+  newGameButton.addEventListener("click", newGame);
+  sizeSlider.addEventListener("input", controlSizeSlider);
+  window.addEventListener("resize", resizeBoard);
+}
+
+function updateDisplay() {
+  scoreDisplay.innerHTML = game.score;
+  sizeDisplay.innerHTML = game.size;
+  sizeSlider.value = game.size;
+  boardDisplay.style.gridTemplate = `repeat(${game.size}, 1fr) / repeat(${game.size}, 1fr)`;
+}
+
+// Key up event controller
+function controlKeyUp(e) {
+  if (game.isGameover) {
+    console.log("gameover!");
+    return;
+  }
+
+  switch (e.keyCode) {
+    case 39:
+      game.moveRight();
+      break;
+    case 37:
+      game.moveLeft();
+      break;
+    case 38:
+      game.moveUp();
+      break;
+    case 40:
+      game.moveDown();
+      break;
+  }
+
+  scoreDisplay.innerHTML = game.score;
+}
+
+function newGame() {
+  game.newGame(sizeSlider.valueAsNumber);
+  updateDisplay();
+}
+
+function controlSizeSlider() {
+  sizeDisplay.innerHTML = sizeSlider.value;
+}
+
+function resizeBoard() {
+  const size = Math.max(
+    250,
+    Math.min(window.innerWidth, window.innerHeight) * boardDisplaySize
+  );
+  boardDisplay.style.width = size + "px";
+  boardDisplay.style.height = size + "px";
+}
+
+// -----------------------------------------------------------------------
+
+start();
